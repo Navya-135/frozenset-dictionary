cb9852e83273b9fffd1abd81d9dc658f48f829d4
Navya-135 <143674020+Navya-135@users.noreply.github.com>
2023-10-19 16:35:19 +0530

    Add files via upload

---
+++ dict.py
@@ -0,0 +1,22 @@
+#dictionay
+
+#implicit
+
+#syntax:variableName={key:value,key:value}
+
+userDetails={'firstName':'Navya','lastName':'Kolakani','Email':'navya@gmail.com'}
+print(type(userDetails))
+
+#Explicit
+
+#syntax:variableName=dict({key:value,key:value})
+
+userDetails=dict({'firstName':'Navya','lastName':'Kolakani','Email':'navya@gmail.com'})
+print(type(userDetails))
+
+#datatype/Variable Annotation
+
+#syntax:variableName:dict={key:value,key:value}
+
+userDetails:dict={'firstName':'Navya','lastName':'Kolakani','Email':'navya@gmail.com'}
+print(type(userDetails))
